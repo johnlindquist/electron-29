2ccecaf353d1e4ab638291291a6e53d88ee3a8ff
John Lindquist <johnlindquist@gmail.com>
2024-02-03 09:27:58 -0700

    esm with electron 29

---
+++ electron.vite.config.ts
@@ -0,0 +1,31 @@
+import { resolve } from 'path'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import react from '@vitejs/plugin-react'
+import { BuildOptions } from 'vite'
+
+const build: BuildOptions = {
+  rollupOptions: {
+    output: {
+      format: 'es'
+    }
+  }
+}
+
+export default defineConfig({
+  main: {
+    build,
+    plugins: [externalizeDepsPlugin()]
+  },
+  preload: {
+    build,
+    plugins: [externalizeDepsPlugin()]
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src')
+      }
+    },
+    plugins: [react()]
+  }
+})
